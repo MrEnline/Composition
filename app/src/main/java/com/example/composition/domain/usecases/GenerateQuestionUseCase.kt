a2cefb3519cf0b38ecc119b45fada13e9f59c53a
Sergey Kuznetsov <mr.enline@gmail.com>
2022-02-08 14:47:23 +0300

    Implementations domain-layer application

---
+++ app/src/main/java/com/example/composition/domain/usecases/GenerateQuestionUseCase.kt
@@ -0,0 +1,21 @@
+package com.example.composition.domain.usecases
+
+import com.example.composition.domain.entity.Question
+import com.example.composition.domain.repository.GameRepository
+
+class GenerateQuestionUseCase (
+    private val repository: GameRepository
+        ) {
+
+    //т.к. классу UseCase нет смысла задавать метод с таким же названием
+    //то можно использовать "operator" и "invoke", чтобы можно было вызывать
+    //класс следующим образом generateQuestionUseCase() или
+    //generateQuestionUseCase.invoke()
+    operator fun invoke(maxSumValue: Int): Question {
+        return repository.generateQuestion(maxSumValue, COUNT_OF_OPTIONS)
+    }
+
+    private companion object {
+        private const val COUNT_OF_OPTIONS = 6;
+    }
+}
