55f9d960325be39f7e84360514cf00f559e23e02
Sergey Kuznetsov <mr.enline@gmail.com>
2022-03-09 18:50:02 +0300

    Начата реализация отображения игрового процесса

--- app/src/main/java/com/example/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.composition.R
 import com.example.composition.databinding.FragmentGameBinding
 import com.example.composition.domain.entity.GameResult
@@ -15,10 +17,13 @@ import java.lang.RuntimeException
 class GameFragment : Fragment() {
 
     private lateinit var level: Level
+    private lateinit var gameViewModel: GameViewModel
 
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding = null")
+    private var buttons: Array<TextView>? = null
+    private var length: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,26 +35,54 @@ class GameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
+        buttons = arrayOf(
+            binding.tvOption1,
+            binding.tvOption2,
+            binding.tvOption3,
+            binding.tvOption4,
+            binding.tvOption5,
+            binding.tvOption6
+        )
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.tvOption1.setOnClickListener {
-            launchGameFinishedFragment(
-                GameResult(true,
-                    0,
-                       0,
-                        GameSettings(0, 0, 0, 0)
-                )
-            )
+        gameViewModel = ViewModelProvider(this)[GameViewModel::class.java]
+        gameViewModel.getGameSettings(level)
+        gameViewModel.question.observe(viewLifecycleOwner) {
+            binding.tvSum.setText(it.sum)
+            binding.tvLeftNumber.setText(it.visibleNumber)
+            length = buttons?.size?.minus(1)!!
+            for(i in 0..length!!) {
+                buttons?.get(i)?.setText(it.options[i+1])
+            }
         }
+        addClickListeners()
+//        binding.tvOption1.setOnClickListener {
+//            launchGameFinishedFragment(
+//                GameResult(true,
+//                    0,
+//                       0,
+//                        GameSettings(0, 0, 0, 0)
+//                )
+//            )
+//        }
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+
+   fun addClickListeners() {
+       for (i in 0..length) {
+           buttons?.get(i)?.setOnClickListener {
+               gameViewModel.generateQuestion()
+           }
+       }
+   }
 
     private fun parseArgs() {
         requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
--- app/src/main/java/com/example/composition/presentation/GameViewModel.kt
+++ app/src/main/java/com/example/composition/presentation/GameViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.composition.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.composition.data.GameRepositoryImpl
 import com.example.composition.domain.entity.GameSettings
@@ -13,14 +15,22 @@ class GameViewModel: ViewModel() {
     private val gameRepository = GameRepositoryImpl
     private val generateQuestionUseCase = GenerateQuestionUseCase(gameRepository)
     private val getGameSettingsUseCase = GetGameSettingsUseCase(gameRepository)
-    private val question: Question? = null
 
-    fun GenerateQuestion(): Question {
-        return generateQuestionUseCase
+    private val _question = MutableLiveData<Question>()
+    val question: LiveData<Question>
+        get() = question
+
+//    private val _gameSettings = MutableLiveData<GameSettings>()
+//    val gameSettings: LiveData<GameSettings>
+//        get() = _gameSettings
+    private lateinit var gameSettings: GameSettings
+
+    fun generateQuestion() {
+        _question.value = generateQuestionUseCase(gameSettings.maxSumValue)
     }
 
-    fun getGameSettings(): GameSettings {
-
+    fun getGameSettings(level: Level) {
+        gameSettings = getGameSettingsUseCase(level)
     }
 
 }
